a567d8abd5fa225389b7f090a12fb2a2e0b7e634
ATOM | lis <atom.bitbucket@gmail.com>
2022-06-18 14:17:10 +0200

    update integration kx21

--- LISserver/aparati/sysmexkx21.js
+++ LISserver/aparati/sysmexkx21.js
@@ -76,7 +76,7 @@ module.exports = {
                                             break;   
                                             case 'HCT':                                                    
                                             temprez = temprez.substring(2,3)+'.'+temprez.substring(3,5)
-                                            temprez = (parseFloat(temprez).toFixed(3)/100).toString()
+                                            temprez = (parseFloat(temprez)/100).toFixed(3).toString()
                                             break;                                    
                                             case 'PLT':                                                    
                                             temprez = temprez.substring(1,4)
@@ -84,17 +84,17 @@ module.exports = {
                                             case 'LYM':                                                    
                                             temprez = temprez.substring(1,3)+'.'+temprez.substring(3,5)
                                             temprez = temprez.replace(/^0+|0+$/g, "")
-                                            temprez = (parseFloat(temprez).toFixed(3)/100).toString()
+                                            temprez = (parseFloat(temprez)/100).toFixed(3).toString()
                                             break;
                                             case 'MIX':                                                    
                                             temprez = temprez.substring(1,3)+'.'+temprez.substring(3,5)
                                             temprez = temprez.replace(/^0+|0+$/g, "")
-                                            temprez = (parseFloat(temprez).toFixed(3)/100).toString()
+                                            temprez = (parseFloat(temprez)/100).toFixed(3).toString()
                                             break;
                                             case 'NEU':                                                    
                                             temprez = temprez.substring(1,3)+'.'+temprez.substring(3,5)
                                             temprez = temprez.replace(/^0+|0+$/g, "")
-                                            temprez = (parseFloat(temprez).toFixed(3)/100).toString()
+                                            temprez = (parseFloat(temprez)/100).toFixed(3).toString()
                                             break;
                                             case 'MIX#':                                                    
                                             temprez = temprez.substring(2,3)+'.'+temprez.substring(3,5)
