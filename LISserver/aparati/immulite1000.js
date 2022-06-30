a95632cee19fe03c3fb0b3aa141c7c32a9959c55
ATOM | lis <atom.bitbucket@gmail.com>
2022-06-30 12:36:08 +0200

    update new fron build

--- LISserver/aparati/immulite1000.js
+++ LISserver/aparati/immulite1000.js
@@ -353,7 +353,7 @@ module.exports = {
                                                       if((anaassay.aparat.sn === json.sn) && (anaassay.test.sifra === test.labassay.sifra)  && (anaassay.test.calculated)){
                                                         test.status_t = "U OBRADI"
                                                       }
-                      if(( (anaassay.test.sifra === test.labassay.sifra)&&(test.status_r ===true) ||( (anaassay.test.sifra === test.labassay.sifra)&&(test.status_t ==="ZAPRIMLJEN")))){
+                      if(( (anaassay.test.sifra === test.labassay.sifra)&&(test.status_r ===true) ||( (anaassay.test.sifra === test.labassay.sifra)&&(test.status_t ==="ZAPRIMLJEN"))||( (anaassay.test.sifra === test.labassay.sifra)&&(test.status_t ==="U OBRADI")))){
                                                           testovi.push(anaassay.kod)
                                                           
                                                           test.status_t = "U OBRADI"
@@ -376,7 +376,7 @@ module.exports = {
                                                       recordret.push(header);
                                                       var query = 'Q|1|^'+json.sid+'||^^^ALL||||||||X'+'\u000D'
                                                       recordret.push(query);
-                                                      var terminator = 'L|1|N'+'\u000D';
+                                                      var terminator = 'L|1|I'+'\u000D';
                                                       recordret.push(terminator);
                                                       callback(recordret);
                                                     }else{
