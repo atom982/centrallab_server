f3549532a6e76ddc9ed44ec67c86d482801d9b65
atom982 <atom982@github.com>
2024-07-15 09:25:45 +0200

    add au 480

--- LISserver/aparati/centaurcp.js
+++ LISserver/aparati/centaurcp.js
@@ -485,7 +485,7 @@ module.exports = {
   
                                     }
                                   });
-
+                                  i++
                                   if(sids.length = i){
                                     var terminator = 'L|1|F'//'\u000D';
                                     recordret.push(terminator);
