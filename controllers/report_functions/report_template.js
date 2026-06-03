7bbe713f53e776e6f417aaa7a8bf1e14cbb66343
ATOM | Lab Software <atom.bitbucket@gmail.com>
2026-06-03 08:43:48 +0200

    05.2026

--- controllers/report_functions/report_template.js
+++ controllers/report_functions/report_template.js
@@ -1494,6 +1494,7 @@ module.exports = {
 
         // doc.moveDown(0.3);
         // doc.font("PTSansBold").fontSize(12).text("Laboratorij pod nadzorom supervizora Butković dr.Nusreta, specijaliste medicinske biohemije");
+        doc.font("PTSansBold").fontSize(12).text("Tačnost i pouzdanost rezultata osigurani su redovnim sprovođenjem eksterne kontrole kvaliteta putem RIQUAS programa (Velika Britanija).");
 
 
         // if (hormoni) {
